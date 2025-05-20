86e7ba2838581b4f41d68ac1632c516613cb2332
tjasim140 <tjasim140@gmail.com>
2025-05-20 10:55:36 -0400

    Added more pieces + set starting positions
    Fixed signals for each piece so they correspond to each piece correctly, added Teddy bear for emotional support

---
+++ Scripts/Piece.gd
@@ -0,0 +1,16 @@
+extends Sprite2D
+var drag = false
+var of = Vector2(0,0)
+
+
+func _process (delta):
+	if drag:
+		position = get_global_mouse_position() - of
+
+func _on_button_button_down() -> void:
+	drag = true
+	of = get_global_mouse_position()-global_position
+	
+
+func _on_button_button_up() -> void:
+	drag = false
