acae7ad2981cd4e7eb5a1ae92fb5d79b9d744906
tjasim140 <tjasim140@gmail.com>
2025-05-20 05:32:09 -0400

    Add files via upload, initial commit
    Added background and piece that is draggable

---
+++ Scripts/piece.gd
@@ -0,0 +1,17 @@
+extends Sprite2D
+var drag = false
+var of = Vector2(0,0)
+
+func _process (dleta):
+	if drag:
+		position = get_global_mouse_position() - of
+
+func _on_button_button_down() -> void:
+	drag = true
+	of = get_global_mouse_position()-global_position
+	
+
+
+func _on_button_button_up() -> void:
+	drag = false
+	
