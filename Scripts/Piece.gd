3a689de3f56d8e79fbcf089a9fdd31e7ed38519a
tjasim140 <tjasim140@gmail.com>
2025-05-27 10:58:20 -0400

    Created new TextureRect, creating spaces on board
    Changing how I approach checking if a certain piece is in a certain spot, creating spaces on board to check if a piece is overlapping with it then it would emit a signal that tells the piece to not move which I will implement next time. Deleted old code checking if piece is at a certain position and created a new TextureRect to be the space on the board

--- Scripts/Piece.gd
+++ Scripts/Piece.gd
@@ -2,16 +2,19 @@ extends Sprite2D
 var drag = false
 var of = Vector2(0,0)
 
-
 func _process (delta):
 	if drag:
 		position = get_global_mouse_position() - of
 
 func _on_button_button_down() -> void:
-	print(position)
-	if position!=Vector2(1140,173):
-		drag = true
-		of = get_global_mouse_position()-global_position
-
+	drag = true
+	of = get_global_mouse_position()-global_position
+	
 func _on_button_button_up() -> void:
 	drag = false
+	
+
+func _on_space_area_entered(area):
+	if area.name == "space":
+		print("BDFIBUWS")
+	 # Replace with function body.
