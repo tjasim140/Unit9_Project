ed4035909b4ab4a087b66d653096cc94a8b2a451
tjasim140 <tjasim140@gmail.com>
2025-05-29 10:59:37 -0400

    Changed size of spaces, added all pieces to group
    All of the spaces need to be connected to pieces through signals

--- Scripts/space.gd
+++ Scripts/space.gd
@@ -1,7 +1,11 @@
 extends TextureRect
 var entered = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 	
+func _on_piece_area_entered(area):
+	if (area.name.contains("space")):
+		remove_from_group("Pieces")
+	# Replace with function body.
 
