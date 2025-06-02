961810b57119eb1b08111349e6db2b74c34f403f
tjasim140 <tjasim140@gmail.com>
2025-06-02 10:59:48 -0400

    Figured out locking pieces, deleted space script
    Figured out how to make each piece stay on the space, deleted space script and created signlas for each space's Area2D space and connected it to each piece and it stays if the piece's name contains "piece" Next time I need to fix the hit boxes so they are smaller and apply the same connection and code to each piece and create more spaces

--- Scenes/piece.gd
+++ Scenes/piece.gd
@@ -1,16 +1,10 @@
-extends Sprite2D
-class_name Board
-
-var hasAll = false
-var count = 0
+extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-	
-	
--- Scripts/Piece.gd
+++ Scripts/Piece.gd
@@ -1,21 +1,24 @@
 extends Sprite2D
+class_name Piece
 var drag = false
+var entered = false
 var of = Vector2(0,0)
 
 func _process (delta):
 	if drag:
-		position = get_global_mouse_position() - of
+		if entered==false:
+			position = get_global_mouse_position() - of
 
 func _on_button_button_down() -> void:
 	drag = true
 	of = get_global_mouse_position()-global_position
 	
-func _on_button_button_up() -> void:
+func _on_button_button_up() -> void:	
 	drag = false
-	
+
 
 func _on_space_area_entered(area):
-	var pos = area.position
-	if area is Space:
+	if area.name.contains("piece"):
+		entered = true
 		drag = false
-		print("working")
+	pass # Replace with function body.
