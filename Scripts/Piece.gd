b4c5ee78bc9ac5c60dedf7003e18eb1d6e3105ba
tjasim140 <tjasim140@gmail.com>
2025-06-04 10:56:29 -0400

    Added all spots on the board and fixed pieces
    Added all spaces to the board, added all signals, and made it so that every piece can snap into place in any space, and they now don't stay stuck in place anymore so now pieces can be moved to another spot after being clicked on again. Next time I want to create a way to show that a piece has made it to the correct place/when all pieces are in specific places then a certain achievement is made

--- Scripts/Piece.gd
+++ Scripts/Piece.gd
@@ -2,6 +2,7 @@ extends Sprite2D
 class_name Piece
 var drag = false
 var entered = false
+var clicked = false
 var of = Vector2(0,0)
 
 func _process (delta):
@@ -9,14 +10,17 @@ func _process (delta):
 		if entered==false:
 			position = get_global_mouse_position() - of
 		
+		
 
 func _on_button_button_down() -> void:
 	drag = true
 	of = get_global_mouse_position()-global_position
+	if entered:
+		drag = true
+		entered = false
 	
 func _on_button_button_up() -> void:	
 	drag = false
-
 
 func _on_space_area_entered(area):
 	if area.name.contains("piece"):
